f79b876665a4b26823ddfa005f299bf6b718c345
NSHBNV <nshbnv@gmail.com>
2022-02-23 18:28:46 +0300

    getters and setters realization

--- src/Point3d.java
+++ src/Point3d.java
@@ -16,4 +16,34 @@ public class Point3d {
         this.yCoord = y;
         this.zCoord = z;
     }
+
+    // Метод получения поля X
+    public double getxCoord() {
+        return xCoord;
+    }
+
+    // Метод получения поля Y
+    public double getyCoord() {
+        return yCoord;
+    }
+
+    // Метод получения поля Z
+    public double getzCoord() {
+        return zCoord;
+    }
+
+    // Метод установки значения X
+    public void setxCoord(double x) {
+        this.xCoord = x;
+    }
+
+    // Метод установки значения Y
+    public void setyCoord(double y) {
+        this.yCoord = y;
+    }
+
+    // Метод установки значения Z
+    public void setzCoord(double z) {
+        this.zCoord = z;
+    }
 }
